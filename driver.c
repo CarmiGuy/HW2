1a2c8ccc2aeace040a44e8d9a64be6ffa64d9a62
CarmiGuy <38726854+CarmiGuy@users.noreply.github.com>
2018-05-09 17:13:30 +0300

    Add files via upload

--- driver.c
+++ driver.c
@@ -1,121 +1,119 @@
 #include "driver.h"
+#include "team.h"
+#include "season.h"
 #include <malloc.h>
 #include <string.h>
 #include <stdio.h>
 
 struct driver
 {
-    int driverId;
-    char* driver_name;
-    Team team;
-    int points;
-    Season season;
+	int driverId;
+	char* driver_name;
+	Team team;
+	int points;
+	Season season;
 };
 
 Driver DriverCreate(DriverStatus* status, char* driver_name, int driverId)
 {
-    char* nameCopy = malloc(sizeof(char) * (strlen(driver_name) + 1));
-    Driver newDriver = malloc(sizeof(newDriver));
-    if(newDriver == NULL || nameCopy == NULL)
-    {
-        *status = DRIVER_MEMORY_ERROR;
-        return NULL;
-    }
-    strcpy(nameCopy, driver_name);
-    newDriver->driver_name = nameCopy;
-    newDriver->driverId = driverId;
-    newDriver->team = NULL;
-    newDriver->season = NULL;
-    *status = DRIVER_STATUS_OK;
-    return newDriver;
+	char* nameCopy = malloc(sizeof(char) * (strlen(driver_name) + 1));
+	Driver newDriver = malloc(sizeof(*newDriver));
+	if (newDriver == NULL || nameCopy == NULL)
+	{
+		*status = DRIVER_MEMORY_ERROR;
+		return NULL;
+	}
+	strcpy_s(nameCopy, strlen(driver_name) + 1, driver_name);
+	newDriver->driver_name = nameCopy;
+	newDriver->driverId = driverId;
+	newDriver->team = NULL;
+	newDriver->season = NULL;
+	*status = DRIVER_STATUS_OK;
+	return newDriver;
 }
 
 void DriverDestroy(Driver driver)
 {
-    free(driver->driver_name);
-   /* free(driver->team); */
-   /* free(driver->season); */
-    free(driver);
+	free(driver->driver_name);
+	/* free(driver->team); */
+	/* free(driver->season); */
+	free(driver);
 }
 
 const char* DriverGetName(Driver driver)
 {
-    if(driver == NULL)
-    {
-        return NULL;
-    }
-    return driver->driver_name;
+	if (driver == NULL)
+	{
+		return NULL;
+	}
+	return driver->driver_name;
 }
 
 int DriverGetId(Driver driver)
 {
-    if(driver == NULL)
-    {
-        return 0;
-    }
-    return driver->driverId;
+	if (driver == NULL)
+	{
+		return 0;
+	}
+	return driver->driverId;
 }
 
 Team DriverGetTeam(Driver driver)
 {
-    if(driver == NULL)
-    {
-        return NULL;
-    }
-    return driver->team;
+	if (driver == NULL)
+	{
+		return NULL;
+	}
+	return driver->team;
 }
 
 void DriverSetTeam(Driver driver, Team team)
 {
-    driver->team = team;
+	driver->team = team;
 }
 
 void DriverSetSeason(Driver driver, Season season)
 {
-    driver->season = season;
-    driver->points = 0;
+	driver->season = season;
+	driver->points = 0;
 }
 
 DriverStatus DriverAddRaceResult(Driver driver, int position)
 {
-    if (driver != NULL)
-    {
-        printf("%s, %d\n", DriverGetName(driver), DriverGetId(driver));
-    }
-    else
-    {
-        printf("bug bug\n");
-    }
-    if(position <= 0)
-    {
-        printf("bug\n");
-        return INVALID_POSITION;
-    }
-    if(driver == NULL)
-    {
-        return INVALID_DRIVER;
-    }
-    printf("%s, %d\n", DriverGetName(driver), DriverGetId(driver));
-    if(driver->season == NULL)
-    {
-        printf("bug bug bug\n");
-        return SEASON_NOT_ASSIGNED;
-    }
-    driver->points += (SeasonGetNumberOfDrivers(driver->season) - position);
-    return DRIVER_STATUS_OK;
+	if (driver != NULL)
+	{
+		printf("%s, %d\n", DriverGetName(driver), DriverGetId(driver));
+	}
+	else
+	{
+		printf("bug bug\n");
+	}
+	if (position <= 0)
+	{
+		printf("bug\n");
+		return INVALID_POSITION;
+	}
+	if (driver == NULL)
+	{
+		return INVALID_DRIVER;
+	}
+	printf("%s, %d\n", DriverGetName(driver), DriverGetId(driver));
+	if (driver->season == NULL)
+	{
+		printf("bug bug bug\n");
+		return SEASON_NOT_ASSIGNED;
+	}
+	driver->points += (SeasonGetNumberOfDrivers(driver->season) - position);
+	return DRIVER_STATUS_OK;
 }
 
 int DriverGetPoints(Driver driver, DriverStatus* status)
 {
-    if(driver == NULL)
-    {
-        *status = INVALID_DRIVER;
-        return 0;
-    }
-    *status = DRIVER_STATUS_OK;
-    return driver->points;
+	if (driver == NULL)
+	{
+		*status = INVALID_DRIVER;
+		return 0;
+	}
+	*status = DRIVER_STATUS_OK;
+	return driver->points;
 }
-
-
-
-
--- team.c
+++ team.c
@@ -5,100 +5,100 @@
 
 struct team
 {
-    char* name;
-    Driver driver1;
-    Driver driver2;
+	char* name;
+	Driver driver1;
+	Driver driver2;
 };
 
 Team TeamCreate(TeamStatus* status, char* name)
 {
-    char* nameCopy = malloc(sizeof(char) * (strlen(name) + 1));
-    Team newTeam = malloc(sizeof(Team*));
-    if(newTeam == NULL || nameCopy == NULL)
-    {
-        *status = TEAM_MEMORY_ERROR;
-        return NULL;
-    }
-    strcpy(nameCopy, name);
-    newTeam->name = nameCopy;
-    newTeam->driver1 = NULL;
-    newTeam->driver2 = NULL;
-    *status = TEAM_STATUS_OK;
-    return newTeam;
+	char* nameCopy = malloc(sizeof(char) * (strlen(name) + 1));
+	Team newTeam = malloc(sizeof(*newTeam));
+	if (newTeam == NULL || nameCopy == NULL)
+	{
+		*status = TEAM_MEMORY_ERROR;
+		return NULL;
+	}
+	strcpy_s(nameCopy, strlen(name) + 1, name);
+	newTeam->name = nameCopy;
+	newTeam->driver1 = NULL;
+	newTeam->driver2 = NULL;
+	*status = TEAM_STATUS_OK;
+	return newTeam;
 }
 
 void TeamDestroy(Team team)
 {
-    free(team->name);
-   /* free(team->driver1);
-    free(team->driver2); */
-    free(team);
+	free(team->name);
+	/* free(team->driver1);
+	free(team->driver2); */
+	free(team);
 }
 
 TeamStatus TeamAddDriver(Team team, Driver driver)
 {
-    if(team == NULL || driver == NULL)
-    {
-        return TEAM_NULL_PTR;
-    }
-    if(TeamGetDriver(team, FIRST_DRIVER) == NULL
-       && TeamGetDriver(team, SECOND_DRIVER) == NULL)
-    {
-        team->driver1 = driver;
-    }
-    else if(TeamGetDriver(team, FIRST_DRIVER) != NULL
-            && TeamGetDriver(team, SECOND_DRIVER) == NULL)
-    {
-        team->driver2 = driver;
-    }
-    else
-    {
-        return TEAM_FULL;
-    }
-    DriverSetTeam(driver, team); //MIGHT NEED TO CHANGE
-    return TEAM_STATUS_OK;
+	if (team == NULL || driver == NULL)
+	{
+		return TEAM_NULL_PTR;
+	}
+	if (TeamGetDriver(team, FIRST_DRIVER) == NULL
+		&& TeamGetDriver(team, SECOND_DRIVER) == NULL)
+	{
+		team->driver1 = driver;
+	}
+	else if (TeamGetDriver(team, FIRST_DRIVER) != NULL
+		&& TeamGetDriver(team, SECOND_DRIVER) == NULL)
+	{
+		team->driver2 = driver;
+	}
+	else
+	{
+		return TEAM_FULL;
+	}
+	DriverSetTeam(driver, team); //MIGHT NEED TO CHANGE
+	return TEAM_STATUS_OK;
 }
 
 const char* TeamGetName(Team team)
 {
-    if(team == NULL)
-    {
-        return NULL;
-    }
-    return team->name;
+	if (team == NULL)
+	{
+		return NULL;
+	}
+	return team->name;
 }
 
 Driver TeamGetDriver(Team team, DriverNumber driver_number)
 {
-    if(team == NULL || (driver_number != FIRST_DRIVER && driver_number != SECOND_DRIVER))
-    {
-        return NULL;
-    }
-    else if(driver_number == FIRST_DRIVER)
-    {
-        return team->driver1;
-    }
-    return team->driver2;
+	if (team == NULL || (driver_number != FIRST_DRIVER && driver_number != SECOND_DRIVER))
+	{
+		return NULL;
+	}
+	else if (driver_number == FIRST_DRIVER)
+	{
+		return team->driver1;
+	}
+	return team->driver2;
 }
 
 int TeamGetPoints(Team team, TeamStatus *status)
 {
-    if(team == NULL)
-    {
-        *status = TEAM_NULL_PTR;
-        return 0;
-    }
-    *status = TEAM_STATUS_OK;
-    DriverStatus driverStatus1 = DRIVER_STATUS_OK, driverStatus2 = DRIVER_STATUS_OK;
-    int firstDriverPoints = DriverGetPoints(team->driver1, &driverStatus1);
-    int secondDriverPoints = DriverGetPoints(team->driver2, &driverStatus2);
-    if(driverStatus1 == DRIVER_STATUS_OK && driverStatus2 == DRIVER_STATUS_OK)
-    {
-        return (firstDriverPoints + secondDriverPoints);
-    }
-    else if(driverStatus1 == DRIVER_STATUS_OK && driverStatus2 == INVALID_DRIVER)
-    {
-        return firstDriverPoints;
-    }
-    return 0;
+	if (team == NULL)
+	{
+		*status = TEAM_NULL_PTR;
+		return 0;
+	}
+	*status = TEAM_STATUS_OK;
+	DriverStatus driverStatus1 = DRIVER_STATUS_OK, driverStatus2 = DRIVER_STATUS_OK;
+	int firstDriverPoints = DriverGetPoints(team->driver1, &driverStatus1);
+	int secondDriverPoints = DriverGetPoints(team->driver2, &driverStatus2);
+	if (driverStatus1 == DRIVER_STATUS_OK && driverStatus2 == DRIVER_STATUS_OK)
+	{
+		return (firstDriverPoints + secondDriverPoints);
+	}
+	else if (driverStatus1 == DRIVER_STATUS_OK && driverStatus2 == INVALID_DRIVER)
+	{
+		return firstDriverPoints;
+	}
+	return 0;
 }
